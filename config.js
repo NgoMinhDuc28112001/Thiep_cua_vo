e7c0190757a59e6d177178f5cde578ac55163309
NgoMinhDucDeveloper <101681994+NgoMinhDuc28112001@users.noreply.github.com>
2024-10-19 09:58:21 +0700

    Add files via upload
    create thiep

---
+++ config.js
@@ -0,0 +1,14 @@
+const nameGirl = 'Vợ (Mỳ Ýy 💞)';
+const giftUrl = '';
+const eventName = 'Chúc Mừng 20-10';
+const titleCard = 'Tặng Mỳ Ý cụa anh';
+const contentCard =
+    'Chúc mừng ngày Phụ nữ Việt Nam 20/10, người yêu thương nhất của anh! Em là người mang đến cho anh niềm vui và hạnh phúc nhất trên thế gian này. Có những lúc đôi ta có những xích mích, nhưng rồi mọi chuyện cũng sẽ ổn. Cảm ơn em vì đã tới bên anh, trao cho anh tình yêu và sự quan tâm vô điều kiện! Yêu em nhiều Mỳ Ý cụa anh💞';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ:
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'anh_vo_yeu.jpg';
+const base64 = '';
+const giftImageBase64 = 'data:image/png;base64, ' + base64;
